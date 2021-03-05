c3a0614e2995c71b4d56ebf9971e5cd211630fd7
mmercadillo <miguel.mercadillo@gmail.com>
2021-03-05 08:01:28 +0100

    Add files via upload

---
+++ daryl-system-robot-rna/src/main/java/daryl/system/robot/rna/apachemq/Receiver.java
@@ -0,0 +1,116 @@
+package daryl.system.robot.rna.apachemq;
+
+import org.slf4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.jms.annotation.JmsListener;
+import org.springframework.jms.config.JmsListenerContainerFactory;
+import org.springframework.stereotype.Component;
+
+import daryl.system.comun.enums.Timeframes;
+import daryl.system.model.Robot;
+import daryl.system.robot.rna.predictor.base.RnaPredictor;
+
+@Component
+public class Receiver {
+	
+	@Autowired
+	Logger logger;
+	@Autowired
+	JmsListenerContainerFactory<?> factory;
+	
+	@Autowired
+	@Qualifier(value = "rnaXauusd60")
+	RnaPredictor rnaXauUsd60;
+	
+	@Autowired
+	@Qualifier(value = "rnaXauusd240")
+	RnaPredictor rnaXauUsd240;
+	
+	@Autowired
+	@Qualifier(value = "rnaXauusd1440")
+	RnaPredictor rnaXauUsd1440;
+	
+	
+	@Autowired
+	@Qualifier(value = "rnaXauusd10080")
+	RnaPredictor rnaXauUsd10080;
+	
+	@Autowired
+	@Qualifier(value = "rnaAudcad60")
+	RnaPredictor rnaAudCad60;
+	
+	@Autowired
+	@Qualifier(value = "rnaAudcad240")
+	RnaPredictor rnaAudCad240;
+	
+	@Autowired
+	@Qualifier(value = "rnaAudcad1440")
+	RnaPredictor rnaAudCad1440;
+	
+	@Autowired
+	@Qualifier(value = "rnaAudcad10080")
+	RnaPredictor rnaAudCad10080;
+	
+	//ndx
+	@Autowired
+	@Qualifier(value = "rnaNdx60")
+	RnaPredictor rnaNdx60;
+	
+	@Autowired
+	@Qualifier(value = "rnaNdx240")
+	RnaPredictor rnaNdx240;
+	
+	@Autowired
+	@Qualifier(value = "rnaNdx1440")
+	RnaPredictor rnaNdx1440;
+	
+	@Autowired
+	@Qualifier(value = "rnaNdx10080")
+	RnaPredictor rnaNdx10080;
+	///////////////////////////////////////
+	
+	//Gdaxi
+	@Autowired
+	@Qualifier(value = "rnaGdaxi60")
+	RnaPredictor rnaGdaxi60;
+
+	@Autowired
+	@Qualifier(value = "rnaGdaxi240")
+	RnaPredictor rnaGdaxi240;
+	
+	@Autowired
+	@Qualifier(value = "rnaGdaxi1440")
+	RnaPredictor rnapGdaxi1440;
+	
+	@Autowired
+	@Qualifier(value = "rnaGdaxi10080")
+	RnaPredictor rnaGdaxi10080;
+	
+
+
+	@JmsListener(destination = "CHNL_RNA")
+	public void receiveMessageGdaxi(Robot robot) {
+		
+		Timeframes timeframe = robot.getTimeframe();
+
+		if(timeframe == Timeframes.PERIOD_H1) {
+			rnaGdaxi60.calculate(robot.getActivo(), robot.getRobot());
+			rnaNdx60.calculate(robot.getActivo(), robot.getRobot());
+			rnaXauUsd60.calculate(robot.getActivo(), robot.getRobot());
+		}else if(timeframe == Timeframes.PERIOD_H4) {
+			rnaGdaxi240.calculate(robot.getActivo(), robot.getRobot());
+			rnaNdx240.calculate(robot.getActivo(), robot.getRobot());
+			rnaXauUsd240.calculate(robot.getActivo(), robot.getRobot());
+		}else if(timeframe == Timeframes.PERIOD_D1) {
+			rnaNdx1440.calculate(robot.getActivo(), robot.getRobot());
+			rnaXauUsd1440.calculate(robot.getActivo(), robot.getRobot());
+		}else if(timeframe == Timeframes.PERIOD_W1) {
+			rnaGdaxi10080.calculate(robot.getActivo(), robot.getRobot());
+			rnaNdx10080.calculate(robot.getActivo(), robot.getRobot());
+			rnaXauUsd10080.calculate(robot.getActivo(), robot.getRobot());
+		}
+		
+	}
+
+}
---
+++ daryl-system-robot-rna/src/main/java/daryl/system/robot/rna/predictor/base/RnaPredictor.java
@@ -0,0 +1,130 @@
+package daryl.system.robot.rna.predictor.base;
+
+import java.util.List;
+
+import org.slf4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Async;
+
+import daryl.system.comun.configuration.ConfigData;
+import daryl.system.comun.enums.Activo;
+import daryl.system.comun.enums.TipoOrden;
+import daryl.system.model.Orden;
+import daryl.system.model.Prediccion;
+import daryl.system.robot.rna.repository.IOrdenRepository;
+import daryl.system.robot.rna.repository.IPrediccionRepository;
+
+public abstract class RnaPredictor {
+
+	@Autowired
+	Logger logger;
+	@Autowired
+	protected ConfigData config;
+		
+	@Autowired
+	protected IOrdenRepository ordenRepository;
+	@Autowired
+	protected IPrediccionRepository prediccionRepository;
+
+	
+	public abstract void calculate(Activo activo, String estrategia);	
+	protected abstract Double calcularPrediccion();
+	//protected abstract Orden calcularOperacion(TipoActivo activo, Estrategia estrategia, Double prediccion);
+
+	//@Async
+	protected void actualizarPrediccionBDs(Activo activo, String estrategia, TipoOrden orden, Double prediccionCierre, Long fechaHoraMillis) {
+		try {
+			
+			//Creamos el bean prediccion
+			Prediccion prediccion = new Prediccion();
+				prediccion.setCierre(prediccionCierre);
+				prediccion.setEstrategia(estrategia);
+				prediccion.setTipoActivo(activo);
+				prediccion.setTipoOrden(orden);
+				prediccion.setFechaHora(fechaHoraMillis);
+				prediccion.setFecha(config.getFechaInString(fechaHoraMillis));
+				prediccion.setHora(config.getHoraInString(fechaHoraMillis));
+				
+			prediccionRepository.save(prediccion);
+			////logger.info("Guardamos la prediccion para {} es {}", activo.name(), prediccion);
+		} catch (Exception e) {
+			logger.error("No se ha podido guardar la prediccion para el activo: {}", activo, e);
+		}
+	}
+	
+	//@Async
+	protected void actualizarUltimaOrden(Activo activo, String estrategia, Orden orden, Long fechaHoraMillis) {
+		try {
+			//Recuperamos la orden sin fecha de fin (fBaja)
+			//logger.info("Buscamos  la orden anterior para {} para actualizar",activo.name());
+			Orden ultimaOrden = ordenRepository.findByfBajaAndTipoActivoAndEstrategia(null, activo, estrategia);
+			if(ultimaOrden != null) {
+				//logger.info("Actualizamos la orden anterior para {} es {}", activo.name(), orden.getTipoOrden());
+				ultimaOrden.setFBaja(fechaHoraMillis);
+				//ordenRepository.saveAndFlush(ultimaOrden);
+				ordenRepository.delete(ultimaOrden);
+				//logger.info("Actualizamos la orden anterior para {} es {}", activo.name(), orden.getTipoOrden());
+				
+			}else {
+				//logger.info("No hay orden para {} para actualziar", activo.name());
+			}
+		}catch (Exception e) {
+			logger.error("No se ha recuperado el valor de la última orden del activo: {}", activo, e);
+		}
+	}
+	
+	@Async
+	protected void guardarNuevaOrden(Orden orden, Long fechaHoraMillis) {
+		try {
+			orden.setFAlta(fechaHoraMillis);
+			ordenRepository.save(orden);
+			//logger.info("Guardamos la orden para {} es {}", orden.getTipoActivo().name(), orden.getTipoOrden());
+		}catch (Exception e) {
+			logger.error("No se ha podido guardar la nueva orden para el activo: {}", orden.getTipoActivo(), e);
+		}
+	}
+	
+	protected final Orden calcularOperacion(Activo activo, String estrategia, Double prediccion, String robot, Boolean inv) {
+		
+		long millis = System.currentTimeMillis();
+		Orden orden = new Orden();
+			orden.setFAlta(System.currentTimeMillis());
+			orden.setFBaja(null);
+			orden.setEstrategia(estrategia);
+			orden.setTipoActivo(activo);
+			orden.setTipoOrden(TipoOrden.CLOSE);
+			orden.setRobot(robot);
+			orden.setFecha(config.getFechaInString(millis));
+			orden.setHora(config.getHoraInString(millis));
+		if(prediccion < 0.0) {
+			orden.setTipoOrden(TipoOrden.SELL);
+			if(inv == Boolean.TRUE) orden.setTipoOrden(TipoOrden.BUY);
+		}else if(prediccion > 0.0) {
+			orden.setTipoOrden(TipoOrden.BUY);
+			if(inv == Boolean.TRUE) orden.setTipoOrden(TipoOrden.SELL);
+		}else {
+			orden.setTipoOrden(TipoOrden.CLOSE);	
+		}
+		
+		return orden;
+	}
+	
+	
+	
+	protected Double media(int periodo, List<Double> hist) {
+		
+		Double media = 0.0;
+		try {
+			List<Double> lista =  hist.subList(hist.size()-periodo, hist.size());
+			Double sum = 0.0;
+			for (Double d : lista) {
+				sum += d;
+			}
+			media = (double)sum/periodo;
+		}catch (Exception e) {
+		
+		}
+		return media;
+	}
+	
+}
