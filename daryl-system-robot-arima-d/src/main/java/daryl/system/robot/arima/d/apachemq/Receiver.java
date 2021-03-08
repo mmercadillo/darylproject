b2b9eaa9a4e8adc7f8cbe5adb841b5ca5bee2f07
mmercadillo <miguel.mercadillo@gmail.com>
2021-03-08 07:40:02 +0100

    Add files via upload

--- daryl-system-robot-arima-d/src/main/java/daryl/system/robot/arima/d/apachemq/Receiver.java
+++ daryl-system-robot-arima-d/src/main/java/daryl/system/robot/arima/d/apachemq/Receiver.java
@@ -120,7 +120,7 @@ public class Receiver {
 	
 	
 	@JmsListener(destination = "CHNL_ARIMA_D")
-	public void receiveMessageGdaxi(Robot robot) {
+	public void receiveMessage(Robot robot) {
 		
 		Timeframes timeframe = robot.getTimeframe();
 
