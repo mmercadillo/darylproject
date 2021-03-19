5a0f33491619fd4d2f75d4d7b1dab5df7b7e9514
Admin <Admin@DESKTOP-BNI6MGG>
2021-03-19 19:09:31 +0100

    DatabaseLoader

---
+++ daryl-system-databaseloader/src/main/java/daryl/system/databaseloader/repository/IHistNdxRepository.java
@@ -0,0 +1,17 @@
+package daryl.system.databaseloader.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import daryl.system.comun.enums.Timeframes;
+import daryl.system.model.historicos.HistNdx;
+
+@Repository
+public interface IHistNdxRepository extends JpaRepository<HistNdx, Long> {
+
+
+	HistNdx findFirstByTimeframeOrderByFechaHoraDesc(Timeframes tf);
+
+}
