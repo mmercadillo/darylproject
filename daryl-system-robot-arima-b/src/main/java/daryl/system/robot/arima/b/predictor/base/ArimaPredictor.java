eb691ccc670d774dc20b0e0d9d6aa9164be4df6f
mmercadillo <mmercadi@pc-mmercadi.ttec.es>
2021-03-22 12:01:07 +0100

    Refactorización y trazas

--- daryl-system-robot-arima-b/src/main/java/daryl/system/robot/arima/b/predictor/base/ArimaPredictor.java
+++ daryl-system-robot-arima-b/src/main/java/daryl/system/robot/arima/b/predictor/base/ArimaPredictor.java
@@ -2,10 +2,8 @@ package daryl.system.robot.arima.b.predictor.base;
 
 import org.slf4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.scheduling.annotation.Async;
 
 import daryl.system.comun.configuration.ConfigData;
-import daryl.system.comun.enums.Activo;
 import daryl.system.comun.enums.TipoOrden;
 import daryl.system.model.Orden;
 import daryl.system.model.Prediccion;
@@ -29,45 +27,40 @@ public abstract class ArimaPredictor {
 	
 	protected abstract Double calcularPrediccion(Robot robot);
 
-
-	private void actualizarPrediccionBDs(Activo activo, String estrategia, String robot, TipoOrden orden, Double prediccionCierre, Long fechaHoraMillis) {
+	private void actualizarPrediccionBDs(Robot robot, TipoOrden orden, Double prediccionCierre, Long fechaHoraMillis) {
 		try {
 			
 			//Creamos el bean prediccion
 			Prediccion prediccion = new Prediccion();
 				prediccion.setCierre(prediccionCierre);
-				prediccion.setEstrategia(estrategia);
-				prediccion.setTipoActivo(activo);
+				prediccion.setEstrategia(robot.getEstrategia());
+				prediccion.setTipoActivo(robot.getActivo());
 				prediccion.setTipoOrden(orden);
 				prediccion.setFechaHora(fechaHoraMillis);
 				prediccion.setFecha(config.getFechaInString(fechaHoraMillis));
 				prediccion.setHora(config.getHoraInString(fechaHoraMillis));
-				prediccion.setRobot(robot);
+				prediccion.setRobot(robot.getRobot());
 				
 			prediccionRepository.save(prediccion);
-			//////logger.info("Guardamos la prediccion para {} es {}", activo.name(), prediccion);
 		} catch (Exception e) {
-			//logger.error("No se ha podido guardar la prediccion para el activo: {}", activo.name(), e);
+			logger.error("No se ha podido guardar la prediccion para el robot: {}", robot.getRobot(), e);
 		}
 	}
 	
-	private void actualizarUltimaOrden(Activo activo, String estrategia, Orden orden, Long fechaHoraMillis) {
+
+	private void actualizarUltimaOrden(Robot robot, Orden orden, Long fechaHoraMillis) {
 		try {
-			//Recuperamos la orden sin fecha de fin (fBaja)
-			////logger.info("Buscamos  la orden anterior para {} para actualizar",activo.name());
-			Orden ultimaOrden = ordenRepository.findByfBajaAndTipoActivoAndEstrategia(null, activo, estrategia);
+
+			Orden ultimaOrden = ordenRepository.findByfBajaAndTipoActivoAndEstrategia(null, robot.getActivo(), robot.getEstrategia());
 			if(ultimaOrden != null) {
-				////logger.info("Actualizamos la orden anterior para {} es {}", activo.name(), orden.getTipoOrden());
 				ultimaOrden.setFBaja(fechaHoraMillis);
-				//ordenRepository.saveAndFlush(ultimaOrden);
 				ordenRepository.delete(ultimaOrden);
-				////logger.info("Actualizamos la orden anterior para {} es {}", activo.name(), orden.getTipoOrden());
 				
 			}else {
-				////logger.info("No hay orden para {} para actualziar", activo.name());
+				logger.info("No hay orden para {} para actualzar del robot", robot.getRobot());
 			}
 		}catch (Exception e) {
-			//logger.error("No se ha recuperado el valor de la última orden del activo: {}", activo.name(), e);
+			logger.error("No se ha recuperado el valor de la última orden del robot: {}", robot.getRobot(), e);
 		}
 	}
 	
@@ -75,22 +68,21 @@ public abstract class ArimaPredictor {
 		try {
 			orden.setFAlta(fechaHoraMillis);
 			ordenRepository.save(orden);
-			////logger.info("Guardamos la orden para {} es {}", orden.getTipoActivo().name(), orden.getTipoOrden());
 		}catch (Exception e) {
-			//logger.error("No se ha podido guardar la nueva orden para el activo: {}", orden.getTipoActivo().name(), e);
+			logger.error("No se ha podido guardar la nueva orden para el robot: {}", orden.getRobot(), e);
 		}
 	}
 	
-	private final Orden calcularOperacion(Activo activo, String estrategia, Double prediccion, String robot, Boolean inv) {
+	private final Orden calcularOperacion(Robot robot, Double prediccion, Boolean inv) {
 		
 		long millis = System.currentTimeMillis();
 		Orden orden = new Orden();
 			orden.setFAlta(millis);
 			orden.setFBaja(null);
-			orden.setEstrategia(estrategia);
-			orden.setTipoActivo(activo);
+			orden.setEstrategia(robot.getEstrategia());
+			orden.setTipoActivo(robot.getActivo());
 			orden.setTipoOrden(TipoOrden.CLOSE);
-			orden.setRobot(robot);
+			orden.setRobot(robot.getRobot());
 			orden.setFecha(config.getFechaInString(millis));
 			orden.setHora(config.getHoraInString(millis));
 		if(prediccion < 0.0) {
@@ -114,18 +106,19 @@ public abstract class ArimaPredictor {
 		logger.info("PREDICCIÓN CALCULADA -> Robot -> " + bot + " Predicción -> " + prediccion);
 		
 		logger.info("SE CALCULA LA ORDEN -> Robot -> " + bot);		
-		Orden orden = calcularOperacion(bot.getActivo(), bot.getEstrategia(), prediccion, bot.getRobot(), bot.getInverso());
+		Orden orden = calcularOperacion(bot, prediccion, bot.getInverso());
 		logger.info("ORDEN CALCULADA -> Robot -> " + bot + " -> Orden -> " + orden);
 		
 		Long fechaHoraMillis = System.currentTimeMillis();
 		
-		actualizarPrediccionBDs(bot.getActivo(), bot.getEstrategia(), bot.getRobot(), orden.getTipoOrden(), prediccion, fechaHoraMillis);
+		actualizarPrediccionBDs(bot, orden.getTipoOrden(), prediccion, fechaHoraMillis);
 		logger.info("PREDICCIÓN ACTUALZIDA -> Robot -> " + bot + " Predicciñon -> " + prediccion);
-		actualizarUltimaOrden(bot.getActivo(), bot.getEstrategia(), orden, fechaHoraMillis);
+		actualizarUltimaOrden(bot, orden, fechaHoraMillis);
 		logger.info("ORDEN ANTERIOR ELIMINADA -> Robot -> " + bot);
 		guardarNuevaOrden(orden, fechaHoraMillis);
 		logger.info("NUEVA ORDEN GUARDADA -> Robot -> " + bot + " -> Orden -> " + orden);
 
 		
 	}
+
 }
