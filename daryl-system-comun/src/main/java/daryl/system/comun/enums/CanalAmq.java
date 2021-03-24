cda98d3bcf57749a6965ddc83a302bdd25ebbf66
Admin <Admin@DESKTOP-BNI6MGG>
2021-03-24 16:52:22 +0100

    CHNL

--- daryl-system-comun/src/main/java/daryl/system/comun/enums/CanalAmq.java
+++ daryl-system-comun/src/main/java/daryl/system/comun/enums/CanalAmq.java
@@ -3,10 +3,15 @@ package daryl.system.comun.enums;
 public enum CanalAmq {
 	
 	CHNL_ARIMA_A,
+	CHNL_ARIMA_A_INV,
 	CHNL_ARIMA_B,
+	CHNL_ARIMA_B_INV,
 	CHNL_ARIMA_C,
+	CHNL_ARIMA_C_INV,
 	CHNL_ARIMA_D,
-	CHNL_RNA;
+	CHNL_ARIMA_D_INV,
+	CHNL_RNA,
+	CHNL_RNA_INV;
 
 	
 	
