04fa371a8b0bdf8f31aa84c1786304feacb0c5c6
mmercadillo <mmercadi@pc-mmercadi.ttec.es>
2021-03-24 12:05:11 +0100

    Unificación de robots Inversos

--- daryl-system-comun/src/main/java/daryl/system/comun/enums/CanalAmq.java
+++ daryl-system-comun/src/main/java/daryl/system/comun/enums/CanalAmq.java
@@ -3,15 +3,10 @@ package daryl.system.comun.enums;
 public enum CanalAmq {
 	
 	CHNL_ARIMA_A,
-	CHNL_ARIMA_A_INV,
 	CHNL_ARIMA_B,
-	CHNL_ARIMA_B_INV,
 	CHNL_ARIMA_C,
-	CHNL_ARIMA_C_INV,
 	CHNL_ARIMA_D,
-	CHNL_ARIMA_D_INV,
-	CHNL_RNA,
-	CHNL_RNA_INV;
+	CHNL_RNA;
 
 	
 	
