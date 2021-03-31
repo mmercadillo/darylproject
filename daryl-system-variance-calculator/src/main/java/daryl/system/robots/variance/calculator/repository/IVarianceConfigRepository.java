50b744382093c66359ad393012d5fa003b55ac82
Admin <Admin@DESKTOP-BNI6MGG>
2021-03-31 19:42:24 +0200

    Añadimos forecaster

---
+++ daryl-system-variance-calculator/src/main/java/daryl/system/robots/variance/calculator/repository/IVarianceConfigRepository.java
@@ -0,0 +1,16 @@
+package daryl.system.robots.variance.calculator.repository;
+
+
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import daryl.system.model.VarianceConfig;
+
+
+@Repository
+public interface IVarianceConfigRepository extends JpaRepository<VarianceConfig, Long> {
+
+	VarianceConfig findVarianceConfigByRobot(String robot);
+
+}
