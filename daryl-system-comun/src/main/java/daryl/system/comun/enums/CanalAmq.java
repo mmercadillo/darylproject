35110ce4565c012ac8dac10eaf7057f2cbc0935b
mmercadi <mmercadi@PC-mmercadi.ttec.es>
2021-04-21 09:11:56 +0200

    Eliminación tipo de robot _I y canal _I

--- daryl-system-comun/src/main/java/daryl/system/comun/enums/CanalAmq.java
+++ daryl-system-comun/src/main/java/daryl/system/comun/enums/CanalAmq.java
@@ -2,17 +2,11 @@ package daryl.system.comun.enums;
 
 public enum CanalAmq {
 	
-	//TODO: Borrar los INV, no se usan
 	CHNL_ARIMA_A,
-	CHNL_ARIMA_A_INV,
 	CHNL_ARIMA_B,
-	CHNL_ARIMA_B_INV,
 	CHNL_ARIMA_C,
-	CHNL_ARIMA_C_INV,
 	CHNL_ARIMA_D,
-	CHNL_ARIMA_D_INV,
 	CHNL_RNA,
-	CHNL_RNA_INV,
 	CHNL_VARIANCE,
 	CHNL_VARIANCE_B;
 	
