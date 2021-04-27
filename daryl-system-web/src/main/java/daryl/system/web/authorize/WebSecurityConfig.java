86bb309fff6a4e951fb9ef40def46db0b05fb0f0
Admin <Admin@DESKTOP-BNI6MGG>
2021-04-27 09:06:35 +0200

    Solución error al calcular la EM

--- daryl-system-web/src/main/java/daryl/system/web/authorize/WebSecurityConfig.java
+++ daryl-system-web/src/main/java/daryl/system/web/authorize/WebSecurityConfig.java
@@ -1,17 +1,58 @@
 package daryl.system.web.authorize;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 @EnableWebSecurity
 @Configuration
 public class WebSecurityConfig  extends WebSecurityConfigurerAdapter {
 	
+	
+	@Override
+	public void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth.inMemoryAuthentication()
+			.withUser("user").password("{noop}password").roles("USER");
+	}
+	
+	
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		
-		http.csrf().disable().authorizeRequests().anyRequest().permitAll();
+		/*
+		http 
+			.csrf() 
+			.disable()
+			//.addFilterBefore(authenticationFilter(), UsernamePasswordAuthenticationFilter.class)
+			//.addFilterAfter(new JWTAuthorizationFilter(), UsernamePasswordAuthenticationFilter.class)
+			.authorizeRequests()
+				//.antMatchers("/assets/**").permitAll()
+				//.antMatchers(HttpMethod.POST, "/login").permitAll()
+				.antMatchers("/**").hasAnyRole("USER")
+				.anyRequest().authenticated()
+				.and()
+			.formLogin().loginPage("/");
+		*/
+
+		/*
+		http.csrf().disable()
+			//.addFilterAfter(new JWTAuthorizationFilter(), UsernamePasswordAuthenticationFilter.class)
+			.authorizeRequests()
+			.antMatchers("/assets/**").permitAll()
+			.antMatchers(HttpMethod.GET, "/**").hasAnyRole("USER")
+			.antMatchers("/").permitAll()
+			.antMatchers(HttpMethod.POST, "/login").permitAll()
+			.and()
+			.formLogin().loginPage("/").permitAll().successForwardUrl("/dashboard")
+            .and()
+            .logout()
+            .permitAll()
+            .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
+            .logoutSuccessUrl("/");
+			*(
 		
 		/*
 		http.csrf().disable()
@@ -28,6 +69,11 @@ public class WebSecurityConfig  extends WebSecurityConfigurerAdapter {
             .logoutSuccessUrl("/");
         */
 		
+	}
+	
+	@Override
+	public void configure(WebSecurity web) throws Exception {
+	    web.ignoring().antMatchers("/assets/**");
 	}
 	
 }
--- daryl-system-web/src/main/java/daryl/system/web/mvc/controller/RobotsCuentaControlller.java
+++ daryl-system-web/src/main/java/daryl/system/web/mvc/controller/RobotsCuentaControlller.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -17,6 +18,7 @@ import daryl.system.web.services.IRobotsCuentaService;
 import daryl.system.web.services.IRobotsService;
 
 @RestController
+@PreAuthorize("hasAuthority('ROLE_USER')")
 public class RobotsCuentaControlller{
 
 	@Autowired
