eab4d94f639a648fe5e31c616098105ae4ba4b7a
Admin <Admin@DESKTOP-BNI6MGG>
2021-05-26 10:05:27 +0200

    Error estrartegia

--- daryl-system-robot-arima-c-2/src/main/java/daryl/system/robot/arima/c2/predictor/base/ArimaPredictor.java
+++ daryl-system-robot-arima-c-2/src/main/java/daryl/system/robot/arima/c2/predictor/base/ArimaPredictor.java
@@ -142,7 +142,8 @@ public abstract class ArimaPredictor {
 			orden.setHora(config.getHoraInString(millis));
 		
 		//recuperamos la orden existente en TF 10080
-		Orden orden10080 = ordenRepository.findByfBajaAndTipoActivoAndEstrategia(null, robot.getActivo(), robot.getArimaConfig());
+		String estrategia = "ARIMA_C_" + robot.getActivo() + "10080";
+		Orden orden10080 = ordenRepository.findByfBajaAndTipoActivoAndEstrategia(null, robot.getActivo(), estrategia);
 			
 		if(orden10080.getTipoOrden() == TipoOrden.SELL) {
 			if(prediccion <= 0.0 && inv == Boolean.FALSE) {
