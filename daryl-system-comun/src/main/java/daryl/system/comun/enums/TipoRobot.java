356ea24bd3565936049072f28c5e34e844feb8f0
mmercadi <mmercadi@PC-mmercadi.ttec.es>
2021-06-02 10:11:40 +0200

    Definición de los robots tipo 3

--- daryl-system-comun/src/main/java/daryl/system/comun/enums/TipoRobot.java
+++ daryl-system-comun/src/main/java/daryl/system/comun/enums/TipoRobot.java
@@ -23,6 +23,14 @@ public enum TipoRobot {
 	ARIMA_B2,
 	ARIMA_B2_I,
 	ARIMA_A2,
-	ARIMA_A2_I;
+	ARIMA_A2_I,
+	ARIMA_D3,
+	ARIMA_D3_I,
+	ARIMA_C3,
+	ARIMA_C3_I,
+	ARIMA_B3,
+	ARIMA_B3_I,
+	ARIMA_A3,
+	ARIMA_A3_I;
 
 }
