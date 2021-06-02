1d5a5225d31e9f1f4f8336a637a76ea3e291de5e
mmercadi <mmercadi@PC-mmercadi.ttec.es>
2021-06-02 13:28:17 +0200

    Robot Variance 3. Versión inicial

---
+++ daryl-system-robot-variance-3/src/main/java/daryl/system/robot/variance3/predictor/Variance3Audcad.java
@@ -0,0 +1,20 @@
+package daryl.system.robot.variance3.predictor;
+
+import org.springframework.beans.factory.config.ConfigurableBeanFactory;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+
+import daryl.system.robot.variance3.predictor.base.Variance3Predictor;
+import lombok.ToString;
+
+@Component
+@Scope(value = ConfigurableBeanFactory.SCOPE_PROTOTYPE)
+@ToString
+public class Variance3Audcad  extends Variance3Predictor{
+
+	
+
+
+
+	
+}
