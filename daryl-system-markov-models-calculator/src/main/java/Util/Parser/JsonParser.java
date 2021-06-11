0898ac6e04f7151330628992bd9c22e132e79be1
mmercadi <mmercadi@PC-mmercadi.ttec.es>
2021-06-11 13:23:20 +0200

    markov

---
+++ daryl-system-markov-models-calculator/src/main/java/Util/Parser/JsonParser.java
@@ -0,0 +1,130 @@
+package Util.Parser;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+/**
+ * Created by Ahmed Hani Ibrahim on 12/19/2015.
+ */
+public class JsonParser {
+    private String data;
+    private JSONObject jsonObject;
+
+    public JsonParser(String filepath) {
+        this.data = this.readFile(filepath);
+        try {
+            this.jsonObject = new JSONObject(data);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Get the json file string data
+     * @return A string which represents the hmm data
+     */
+    public String getData() {
+        return this.data;
+    }
+
+    /**
+     * Get the model name
+     * @return A String which is the model name
+     * @throws JSONException
+     */
+
+    public String getName() throws JSONException {
+        return this.jsonObject.getJSONObject("modelInfo").getString("name");
+    }
+
+    /**
+     * Get the model creation date
+     * @return A String which is the model creation date
+     * @throws JSONException
+     */
+
+    public String getCreationTime() throws JSONException {
+        return this.jsonObject.getJSONObject("modelInfo").getString("created_at");
+    }
+
+    /**
+     * Get the model creation purpose
+     * @return A String which is the model creation purpose
+     * @throws JSONException
+     */
+
+    public String getPurpose() throws JSONException {
+        return this.jsonObject.getJSONObject("modelInfo").getString("for");
+    }
+
+    /**
+     * Get the model states
+     * @return A String which is the model states
+     * @throws JSONException
+     */
+
+    public String getStates() throws JSONException {
+        return this.jsonObject.getJSONObject("modelData").getString("states");
+    }
+
+    /**
+     * Get the model initial probabilities of the states
+     * @return A String which is the model initial probabilities of the states
+     * @throws JSONException
+     */
+
+    public String getInitialProbabilities() throws JSONException {
+        return this.jsonObject.getJSONObject("modelData").getString("initial_prop");
+    }
+
+    /**
+     * Get the model observations
+     * @return A String which is the model observations
+     * @throws JSONException
+     */
+
+    public String getObservations() throws JSONException {
+        return this.jsonObject.getJSONObject("modelData").getString("observations");
+    }
+
+    /**
+     * Get the model transition matrix
+     * @return A String which represents the transition between the model states
+     * @throws JSONException
+     */
+
+    public String getTransitionMatrix() throws JSONException {
+        return this.jsonObject.getJSONObject("modelData").getString("transition_matrix");
+    }
+
+    /**
+     * Get the model emission matrix
+     * @return A String which represents the emission matrix between the states and observations
+     * @throws JSONException
+     */
+
+    public String getEmissionMatrix() throws JSONException {
+        return this.jsonObject.getJSONObject("modelData").getString("emission_matrix");
+    }
+
+
+    private String readFile(String filename) {
+        String result = "";
+        try {
+            BufferedReader br = new BufferedReader(new FileReader(filename));
+            StringBuilder sb = new StringBuilder();
+            String line = br.readLine();
+            while (line != null) {
+                sb.append(line);
+                line = br.readLine();
+            }
+            result = sb.toString();
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
+        return result;
+    }
+}
---
+++ daryl-system-markov-models-calculator/src/main/java/Util/Validation/Validator.java
@@ -0,0 +1,124 @@
+package Util.Validation;
+
+import java.util.Hashtable;
+import java.util.Vector;
+
+import ML.HMM.Pair;
+
+/**
+ * Created by Ahmed Hani Ibrahim on 12/19/2015.
+ */
+public class Validator {
+    private static Validator ourInstance = new Validator();
+
+    public static Validator getInstance() {
+        return ourInstance;
+    }
+
+    private Validator() {
+    }
+
+    public boolean summationIsOne(Hashtable<String, Double> list) {
+        double sum = 0.0;
+
+        for (double item : list.values()) {
+            sum += item;
+        }
+
+        return sum == 1.0;
+    }
+
+    public boolean isValidInitialProbabilities(Vector<String> states, Hashtable<String, Double> initialProbabilities) {
+        if (states.size() != initialProbabilities.size())
+            return false;
+
+        for (int i = 0; i < states.size(); i++) {
+            boolean found = false;
+            for (String state : initialProbabilities.keySet()) {
+                if (state.equals(states.get(i))) {
+                    found = true;
+                    break;
+                }
+            }
+
+            if (!found)
+                return false;
+        }
+
+        return true;
+    }
+
+    public boolean isValidTransitionMatrix(Hashtable<Pair<String, String>, Double> transitionMatrix, Vector<String> states) {
+        if (transitionMatrix.size() != states.size() * states.size())
+            return false;
+
+        Hashtable<Pair<String, String>, Boolean> frequency = new Hashtable<Pair<String, String>, Boolean>();
+
+        for (Pair<String, String> item : transitionMatrix.keySet()) {
+            if (frequency.containsKey(item)) {
+                return false;
+            }
+            frequency.put(item, true);
+        }
+
+        Hashtable<Pair<String, String>, Boolean> visited = new Hashtable<Pair<String, String>, Boolean>();
+
+        for (Pair<String, String> first : transitionMatrix.keySet()) {
+            double sum = 0.0;
+            int entered = 0;
+            String state = first.getKey();
+            for (Pair<String, String> second: transitionMatrix.keySet()) {
+                if (state.equals(second.getKey()) && !visited.containsKey(second)) {
+                    sum += transitionMatrix.get(second);
+                    entered++;
+                    visited.put(second, true);
+                }
+            }
+
+            //if (sum != 1.0 && entered > 0) {
+            if (sum > 1.0 && entered > 0) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    public boolean isValidEmissionMatrix(Hashtable<Pair<String, String>, Double> emissionMatrix, Vector<String> states, Vector<String> observations) {
+        if (emissionMatrix.size() != observations.size() * states.size()) {
+            return false;
+        }
+
+        for (Pair<String, String> item : emissionMatrix.keySet()) {
+            boolean found = false;
+            double sum = 0.0;
+            int count = 0;
+            for (int i = 0; i < states.size(); i++) {
+                for (int j = 0; j < observations.size(); j++) {
+                    if (item.getKey().equals(states.get(i)) && item.getValue().equals(observations.get(j))) {
+                        found = true;
+                        break;
+                    }
+                }
+
+                if (found)
+                    break;
+            }
+
+            if (!found)
+                return false;
+
+            for (Pair<String, String> item2 : emissionMatrix.keySet()) {
+                if (item.getKey().equals(item2.getKey())) {
+                    sum += emissionMatrix.get(item2);
+                    count++;
+                }
+            }
+
+            if (sum > 1.0 && count > 0)
+                return false;
+        }
+
+        return true;
+    }
+}
