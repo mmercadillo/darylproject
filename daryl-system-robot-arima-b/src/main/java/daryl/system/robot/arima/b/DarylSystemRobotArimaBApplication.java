d19f08a53eda92bcb241ae3cb939043319cdad53
mmercadillo <miguel.mercadillo@gmail.com>
2021-06-14 17:54:12 +0200

    jms

--- daryl-system-robot-arima-b/src/main/java/daryl/system/robot/arima/b/DarylSystemRobotArimaBApplication.java
+++ daryl-system-robot-arima-b/src/main/java/daryl/system/robot/arima/b/DarylSystemRobotArimaBApplication.java
@@ -2,7 +2,6 @@ package daryl.system.robot.arima.b;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.builder.SpringApplicationBuilder;
