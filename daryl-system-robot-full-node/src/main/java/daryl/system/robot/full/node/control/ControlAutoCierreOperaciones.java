9b3693cb76aca678b7f47ee69735adb17d2a8caf
mmercadillo <miguel.mercadillo@gmail.com>
2021-06-15 17:35:25 +0200

    TEST

--- daryl-system-robot-full-node/src/main/java/daryl/system/robot/full/node/control/ControlAutoCierreOperaciones.java
+++ daryl-system-robot-full-node/src/main/java/daryl/system/robot/full/node/control/ControlAutoCierreOperaciones.java
@@ -57,6 +57,8 @@ public class ControlAutoCierreOperaciones {
 				}catch (Exception e) {
 					logger.error(e.getMessage(), e);
 				}
+			}else {
+				logger.info("NO SE DAN LAS CONDICIONES PARA CERRAR OPERACIONES=============");
 			}
     	}
 		
