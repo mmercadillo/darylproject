914bcb034fbde99e6bd01c36f526857018433915
mmercadillo <miguel.mercadillo@gmail.com>
2021-06-15 20:43:40 +0200

    Full node

--- daryl-system-robot-full-node/src/main/java/daryl/system/robot/full/node/predictor/variance/Variance3Forecaster.java
+++ daryl-system-robot-full-node/src/main/java/daryl/system/robot/full/node/predictor/variance/Variance3Forecaster.java
@@ -1,19 +1,90 @@
 package daryl.system.robot.full.node.predictor.variance;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.config.ConfigurableBeanFactory;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
+import org.ta4j.core.BarSeries;
+import org.ta4j.core.MaxMinNormalizer;
+import org.ta4j.core.utils.BarSeriesUtils;
 
+import daryl.system.comun.enums.Mode;
 import daryl.system.comun.enums.TipoOrden;
 import daryl.system.model.Orden;
 import daryl.system.model.Robot;
+import daryl.system.model.VarianceConfig;
+import daryl.system.model.historicos.Historico;
+import daryl.system.robot.full.node.predictor.base.Forecaster;
+import daryl.system.robot.full.node.repository.IVarianceConfigRepository;
+import daryl.variance.StockPredict;
 import lombok.ToString;
 
 @Component
 @Scope(value = ConfigurableBeanFactory.SCOPE_PROTOTYPE)
 @ToString
-public class Variance3Forecaster  extends VarianceForecaster{
+public class Variance3Forecaster  extends Forecaster{
 
+	
+	@Autowired
+	private IVarianceConfigRepository varianceConfigRepository;
+
+	@Override
+	protected Double calcularPrediccion(Robot bot) {
+		
+		Double prediccion = 0.0;
+		
+		
+		List<Historico> historico = historicoRepository.findAllByTimeframeAndActivoOrderByFechaHoraAsc(bot.getTimeframe(), bot.getActivo());
+		BarSeries serieParaCalculo = BarSeriesUtils.generateBarListFromHistorico(historico,  "BarSeries_" + bot.getTimeframe() + "_" + bot.getActivo(), bot.getActivo().getMultiplicador());
+		MaxMinNormalizer darylNormalizer =  new MaxMinNormalizer(serieParaCalculo, Mode.CLOSE);
+		List<Double> datos = darylNormalizer.getDatos();
+
+
+		
+		try {
+			
+			VarianceConfig varianceConfig = varianceConfigRepository.findVarianceConfigByRobot(bot.getVarianceConfig());
+			if(varianceConfig != null) {
+				
+				int n = varianceConfig.getN();
+				int offset = varianceConfig.getOffset();
+				double alpha = varianceConfig.getAlpha();
+				double beta = varianceConfig.getBeta();
+				int m = varianceConfig.getLastM();
+				
+				try {
+	        		
+	        		StockPredict stock = new StockPredict(datos, offset, n, alpha, beta, m);
+	        		double[] priceVariance = stock.getPriceVariance();
+	        		
+	        		double forecast = priceVariance[0];
+	        		logger.info("Robot -> " + bot.getRobot() + " PREDICCIÓN -> " + forecast + " ANTERIOR -> " + datos.get(datos.size()-1));
+	        		if(forecast > datos.get(datos.size()-1)) {
+			        	prediccion = 1.0;
+			        }
+			        if(forecast < datos.get(datos.size()-1)) {
+			        	prediccion = -1.0;
+			        }
+	        		
+	        	}catch (Exception e) {
+	        		logger.error("No se ha podido calcular la prediccion para el robot: {}", bot.getRobot(), e);
+	        	}
+				
+			}else {
+				logger.info("No existe config para el robot: {}", bot.getRobot());
+			}
+			
+		}catch (Exception e) {
+			logger.error("No se ha podido calcular la prediccion para el robot: {}", bot.getRobot(), e);
+		}
+
+		return prediccion;
+		
+	}
+	
+	
 	@Override
 	protected Orden calcularOperacion(Robot robot, Double prediccion, Boolean inv) {
 		
