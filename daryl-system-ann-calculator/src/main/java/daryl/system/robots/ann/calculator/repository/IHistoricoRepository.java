b27413700a6244ca0282bb2d74aae53203bde781
mmercadillo <miguel.mercadillo@gmail.com>
2021-06-17 15:56:14 +0200

    ANN Calculator version inicial

---
+++ daryl-system-ann-calculator/src/main/java/daryl/system/robots/ann/calculator/repository/IHistoricoRepository.java
@@ -0,0 +1,20 @@
+package daryl.system.robots.ann.calculator.repository;
+
+import java.util.List;
+
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import daryl.system.comun.enums.Activo;
+import daryl.system.comun.enums.Timeframes;
+import daryl.system.model.historicos.Historico;
+
+@Repository
+public interface IHistoricoRepository extends JpaRepository<Historico, Long> {
+
+	Historico findFirstByTimeframeAndActivoOrderByFechaHoraDesc(Timeframes tf, Activo activo);
+	List<Historico> findAllByTimeframeAndActivoOrderByFechaHoraAsc(Timeframes tf, Activo activo, Pageable page);
+	List<Historico> findAllByTimeframeAndActivoOrderByFechaHoraAsc(Timeframes tf, Activo activo);
+	
+}
