9f47c47c7eff35432e4e07c9a07fa8eae7754cd8
mmercadillo <miguel.mercadillo@gmail.com>
2021-06-18 09:14:27 +0200

    limpieza

--- daryl-system-robot-full-node/src/main/java/daryl/system/robot/full/node/DarylSystemRobotFullNodeApplication.java
+++ daryl-system-robot-full-node/src/main/java/daryl/system/robot/full/node/DarylSystemRobotFullNodeApplication.java
@@ -1,7 +1,5 @@
 package daryl.system.robot.full.node;
 
-import java.util.concurrent.Executor;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -11,9 +9,7 @@ import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.jms.annotation.EnableJms;
-import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
-import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication(scanBasePackages = {"daryl.system"})
@@ -22,7 +18,6 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 @EnableJms
 @EnableTransactionManagement
 @EnableScheduling
-@EnableAsync
 public class DarylSystemRobotFullNodeApplication {
 
 	
