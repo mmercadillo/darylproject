408bdb81789499d25e4ea5aeb3ed75953d5caf3f
mmercadillo <miguel.mercadillo@gmail.com>
2021-06-18 12:29:19 +0200

    Ann robot

--- daryl-system-robot-full-node/src/main/java/daryl/system/robot/full/node/predictor/ann/AnnForecaster.java
+++ daryl-system-robot-full-node/src/main/java/daryl/system/robot/full/node/predictor/ann/AnnForecaster.java
@@ -1,8 +1,19 @@
 package daryl.system.robot.full.node.predictor.ann;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import java.io.IOException;
+import java.util.List;
 
+import org.apache.commons.lang3.SerializationUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
+import org.ta4j.core.BarSeries;
+import org.ta4j.core.MaxMinNormalizer;
+import org.ta4j.core.utils.BarSeriesUtils;
+
+import daryl.system.comun.enums.Mode;
+import daryl.system.model.AnnConfig;
 import daryl.system.model.Robot;
+import daryl.system.model.historicos.Historico;
 import daryl.system.robot.full.node.predictor.base.Forecaster;
 import daryl.system.robot.full.node.repository.IAnnConfigRepository;
 
@@ -18,10 +29,77 @@ public class AnnForecaster  extends Forecaster{
 	public Double calcularPrediccion(Robot bot){
 
 		Double prediccion = 0.0;
+		AnnConfig annConfig = getAnnConfig(bot);
 
+		if(annConfig != null) {
+			
+			ANN ann  = null;
+			try {
+				ann = annFromByteArray(annConfig.getAnn());
+			} catch (ClassNotFoundException | IOException e1) {
+				e1.printStackTrace();
+			}
+			
+			if(ann != null) {		
+				
+				List<Historico> historico = historicoRepository.findAllByTimeframeAndActivoOrderByFechaHoraAsc(bot.getTimeframe(), bot.getActivo(), PageRequest.of(0,  annConfig.getNeuronasEntrada()));
+				BarSeries serieParaCalculo = BarSeriesUtils.generateBarListFromHistorico(historico,  "BarSeries_" + bot.getTimeframe() + "_" + bot.getActivo(), bot.getActivo().getMultiplicador());
+				MaxMinNormalizer darylNormalizer =  new MaxMinNormalizer(serieParaCalculo, Mode.CLOSE);
+				List<Double> datos = darylNormalizer.getDatos();
+				
+
+				prediccion = prediccionANN(ann, datos);
+				
+				
+			}
+			
+		}
+		
 		
         return prediccion;
 	
+	}
+	
+    private double prediccionANN(ANN net, List<Double> datos){
+    	
+    	double prediccion = 0.0;
+    	
+    	try {
+	    	double[] input = datos.stream().mapToDouble(dato -> dato.doubleValue()).toArray();
+	    	
+	        FischerTransform ft_ann = new FischerTransform();
+	        MovingAverages ma = new MovingAverages();
+	        
+	        input = ft_ann.convert(input);
+	        input = ma.SMA(input, 5);
+	        
+	        double[] annSignalTemp = net.run(input);
+	        long annSignal = Math.round(annSignalTemp[0]);
+	        
+	        if (annSignal == 0.0) {
+	        	//Vendemos
+	        	prediccion = -1.0;
+	        }
+	        else if (annSignal == 1.0){
+	        	//Compramos
+	        	prediccion = 1.0;
+	        }
+    	}catch (Exception e) {
+		}
+        return prediccion;
+    }
+	
+	
+	private AnnConfig getAnnConfig(Robot robot) {
+		
+		return annConfigRepository.findAnnConfigByRobot(robot.getAnnConfig());
+		
+	}
+	
+	public ANN annFromByteArray(byte[] byteArray) throws IOException, ClassNotFoundException {
+		
+		ANN ann = (ANN)SerializationUtils.deserialize(byteArray);
+		return ann;
 	}
 
 
