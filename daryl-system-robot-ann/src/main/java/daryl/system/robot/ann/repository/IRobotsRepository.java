211d4ab4ff39509f060f47670874185074a58dc8
mmercadillo <miguel.mercadillo@gmail.com>
2021-06-20 18:07:59 +0200

    Ann robot

---
+++ daryl-system-robot-ann/src/main/java/daryl/system/robot/ann/repository/IRobotsRepository.java
@@ -0,0 +1,19 @@
+package daryl.system.robot.ann.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import daryl.system.comun.enums.Activo;
+import daryl.system.comun.enums.Timeframes;
+import daryl.system.model.Robot;
+
+@Repository
+public interface IRobotsRepository extends JpaRepository<Robot, Long> {
+
+	@Query("SELECT ro FROM Robot ro WHERE ro.activo = ?1 and ro.timeframe = ?2")
+	List<Robot> findRobotsByActivoAndTimeframe(Activo activo, Timeframes timeframe);
+
+}
